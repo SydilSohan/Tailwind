d505811540ec832ea8abfba5293465eff8cda419
Sydil Sohan <mdsohan383@gmail.com>
2022-09-24 11:50:58 +0600

    initial-com

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  return (
+    <div className="w-10/12 h-screen items-center text-center self-center mx-auto my-12">
+      <h2 className="text-center text-6xl  text-green animate-bounce">Sydil's World</h2>
+      <div className="container grid  grid-flow-row grid-cols-3 gap-2 sm:gap-6 align-middle duration-200 ease-in-out child:p-4">
+        <div className="item1 shadow-xl grid p-2 sm:p-4 align-middle col-span-2  bg-gray-light border-gray-dark border-solid border-2  rounded-lg hover:bg-gray-dark duration-200 ease-in-out " ><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, aliquam temporibus itaque doloribus molestiae mollitia consequuntur maxime rerum commodi culpa eaque accusantium, quis architecto sint explicabo distinctio nostrum. Sint, nam.</p></div>
+        <div className="item1 shadow-xl gird  p-2 sm:p-4 grid-flow-col align-bottom  bg-gray-light border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out ">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle  bg-gray-light border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        <div className="item1 shadow-xl grid p-2 sm:p-4  grid-flow-col align-middle   border-gray-dark border-solid border-2 rounded-lg hover:bg-gray-dark duration-200 ease-in-out">Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa voluptate dolore, suscipit eos nam molestiae impedit sequi. Atque neque eum ipsam ab natus ullam non impedit doloremque sapiente quasi!</div>
+        
+
+      </div>
+      <div className="p-12"></div>
+    </div>
+  );
+};
+
+export default App;
